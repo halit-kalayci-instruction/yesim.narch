78524fd5630a1b089156e5392dffec3a62ae390e
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-11-30 12:15:08 +0300

    add client files

--- RentACar-Udemy-Course/WebApi/Program.cs
+++ RentACar-Udemy-Course/WebApi/Program.cs
@@ -3,8 +3,10 @@ using Core.CrossCuttingConcerns.Exceptions.Extensions;
 using Core.Security;
 using Core.Security.Encryption;
 using Core.Security.JWT;
+using Core.WebAPI.Extensions.Swagger;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,7 +17,6 @@ builder.Services.AddApplicationServices();
 builder.Services.AddSecurityServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
 
 // appSettings.json'dan tokenoptions'ý oku
@@ -40,6 +41,24 @@ builder.Services
         };
     });
 
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.AddSecurityDefinition(
+        name: "Bearer",
+        securityScheme: new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description =
+                "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer YOUR_TOKEN\". \r\n\r\n"
+                + "`Enter your token in the text input below.`"
+        }
+    );
+    opt.OperationFilter<BearerSecurityRequirementOperationFilter>();
+});
 
 var app = builder.Build();
 
