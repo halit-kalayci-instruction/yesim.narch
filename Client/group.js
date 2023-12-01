cc6ce69e4be7aa363f14c0890c621f455a0ab0ef
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-01 16:04:26 +0300

    groups

--- Client/group.js
+++ Client/group.js
@@ -1,5 +1,11 @@
 const groupInput = document.querySelector("#group")
-const button = document.querySelector("#joinBtn")
+const connectBtn = document.querySelector("#connectBtn")
+const tokenInput = document.querySelector("#token");
+
+
+const select = document.querySelector("#roleSelect");
+const messageInput = document.querySelector("#message");
+const sendBtn = document.querySelector("#sendBtn");
 
 const connection = new signalR
     .HubConnectionBuilder()
@@ -7,17 +13,19 @@ const connection = new signalR
         {
             skipNegotiation: true,
             transport: signalR.HttpTransportType.WebSockets,
-            // accessTokenFactory: () => { return tokenInput.value }
+            accessTokenFactory: () => { return tokenInput.value }
         })
     .withAutomaticReconnect([5000, 5000])
     .build();
 
 connection.on('NewUser', (info) => console.log(info));
 
-connection.start().then(() => {
+connection.on('MessageReceived', (message) => console.log("Yeni bir duyuru var: ", message))
 
-});
+connectBtn.addEventListener('click', () => {
+    connection.start().then(() => { })
+})
 
-button.addEventListener('click', () => {
-    connection.invoke("JoinGroup", groupInput.value)
+sendBtn.addEventListener('click', () => {
+    connection.invoke("SendMessage", { group: select.value, message: messageInput.value })
 })
--- RentACar-Udemy-Course/Application/Hubs/NotificationHub.cs
+++ RentACar-Udemy-Course/Application/Hubs/NotificationHub.cs
@@ -5,14 +5,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Security.Extensions;
 
 namespace Application.Hubs
 {
+    [Authorize]
     public class NotificationHub : Hub
     {
-        public override Task OnConnectedAsync()
+        public override async Task OnConnectedAsync()
         {
-            return base.OnConnectedAsync();
+            var roles = Context.User.ClaimRoles();
+            foreach (var role in roles)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, role);
+            }
+            base.OnConnectedAsync();
         }
 
         public async Task JoinGroup(string groupName)
@@ -23,6 +30,16 @@ namespace Application.Hubs
            // Gruptaki çağrıyı yapan client dışındaki tüm elemanlar => OthersInGroup
            await Clients.Group(groupName).SendAsync("NewUser", Context.ConnectionId + " has joined to group");
         }
+
+        public async Task SendMessage(SendNotificationModel message)
+        {
+            await Clients.Group(message.Group).SendAsync("MessageReceived", message.Message);
+        }
+    }
+
+    public class SendNotificationModel
+    {
+        public string Group { get; set; }
+        public string Message { get; set; }
     }
 }
-// 3:45
