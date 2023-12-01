67a1d434efe9758ce966f659168822bb590caee8
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-01 14:40:37 +0300

    callback functions signalR

--- Client/notifications.js
+++ Client/notifications.js
@@ -1,3 +1,5 @@
+const statusDiv = document.querySelector("#connectionStatus")
+
 const connection = new signalR
     .HubConnectionBuilder()
     .withUrl("https://localhost:7285/api/notificationhub",
@@ -6,9 +8,27 @@ const connection = new signalR
             transport: signalR.HttpTransportType.WebSockets,
             // accessTokenFactory: () => { return tokenInput.value }
         })
-    .withAutomaticReconnect([5000, 10000, 15000])
+    .withAutomaticReconnect([5000, 5000])
     .build();
+
+connection.onclose(() => {
+    statusDiv.innerHTML = "Bağlantı koptu..";
+    statusDiv.className = "red";
+})
+
+connection.onreconnected((arg) => {
+    statusDiv.innerHTML = "Bağlandı";
+    statusDiv.className = "green";
+})
+
+connection.onreconnecting((arg) => {
+    statusDiv.innerHTML = "Bağlanıyor";
+    statusDiv.className = "orange";
+})
 
 connection.on('NewBrandAdded', (brand) => console.log(brand));
 
-connection.start();
+connection.start().then(() => {
+    statusDiv.innerHTML = "Bağlandı";
+    statusDiv.className = "green";
+});
