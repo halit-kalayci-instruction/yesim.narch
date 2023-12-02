994f2c149cc18311c769193bd73a2f734e1c0a65
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-02 10:53:29 +0300

    js in jsx

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -4,15 +4,28 @@ import './App.css';
 
 // JSX => Tüm jsx yapısı tek bir parent'in altında olmalıdır.
 // React.Fragment => <> </>
+
+// {} => içerisinde JS kodu yazılacak
+// arrow function syntax
 function App() {
+  let name = "Halit";
+  let show = true;
+  let list = ["a", "b", "c"]
+
   return (
     <>
       <div className="App">
-        <h3>Merhaba</h3>
+        <h3>{name}</h3>
       </div>
-      <div>
-        Merhaba
-      </div>
+      {show ? <div>True</div> : <div>False</div>}
+
+      <ul>
+        {/* {list.map(element => <li>{element}</li>)} */}
+        {list.map(element => {
+          return <li>{element}</li>
+        }
+        )}
+      </ul>
     </>
   );
 }
