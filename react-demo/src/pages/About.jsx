eb9aec104a7d04ce4c3e7fa84f779424f0f73056
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-02 12:25:23 +0300

    two way data binding

--- react-demo/src/pages/About.jsx
+++ react-demo/src/pages/About.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 function About() {
-	return <div>About</div>;
+	const [name, setName] = useState("");
+
+	const changeName = () => {
+		setName("Halit");
+	};
+
+	// Two Way Data Binding
+	// değişken değişirse inputun içi değişsin
+	// input değişirse değişken değişsin
+	return (
+		<div>
+			<form>
+				<input
+					type="text"
+					placeholder="Ad"
+					value={name}
+					onChange={e => setName(e.target.value)}
+				/>
+				<button type="button" onClick={() => console.log(name)}>
+					Gönder
+				</button>
+				<button type="button" onClick={() => changeName()}>
+					Değiştir
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default About;
--- react-demo/src/pages/Homepage.jsx
+++ react-demo/src/pages/Homepage.jsx
@@ -3,21 +3,24 @@ import {Link} from "react-router-dom";
 
 // HREF kullanımı yanlış.
 function Homepage() {
+	const [count2, setCount2] = useState(0);
+	const [count3, setCount3] = useState(0);
 	// hooks
 	// 2. parametre => Dep List.
 	useEffect(() => {
 		console.log("Sayfanın ilk yüklenme anı");
 	}, []);
-
-	let [count2, setCount2] = useState(0);
-
 	// değiştiğinde state'i (UI) değiştirmek istediğiniz değişkenleri
 	// useState
 
 	// useState ile tanımlı bir değişkenin değerinin değiştiği anı izlemek istersek
 	useEffect(() => {
 		console.log(count2);
-	}, [count2]);
+		console.log(count3);
+	}, [count2, count3]);
+
+	const submitForm = () => {};
+
 	return (
 		<div>
 			<p>{count2}</p>
@@ -27,6 +30,15 @@ function Homepage() {
 				}}
 			>
 				Artır
+			</button>
+
+			<p>{count3}</p>
+			<button
+				onClick={() => {
+					setCount3(prevState => prevState + 1);
+				}}
+			>
+				Artır 3
 			</button>
 			<Link to="/about">About'a Git</Link>
 		</div>
