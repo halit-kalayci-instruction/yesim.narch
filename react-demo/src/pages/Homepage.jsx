e5b4143e9c2cf03c38806dc2ae636c583dced0c1
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-02 12:09:21 +0300

    hooks

--- react-demo/src/pages/Homepage.jsx
+++ react-demo/src/pages/Homepage.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 
 // HREF kullanımı yanlış.
 function Homepage() {
+	// hooks
+	// 2. parametre => Dep List.
+	useEffect(() => {
+		console.log("Sayfanın ilk yüklenme anı");
+	}, []);
+
+	let [count2, setCount2] = useState(0);
+
+	// değiştiğinde state'i (UI) değiştirmek istediğiniz değişkenleri
+	// useState
+
+	// useState ile tanımlı bir değişkenin değerinin değiştiği anı izlemek istersek
+	useEffect(() => {
+		console.log(count2);
+	}, [count2]);
 	return (
 		<div>
-			<p>Homepage</p>
+			<p>{count2}</p>
+			<button
+				onClick={() => {
+					setCount2(prevState => prevState + 1);
+				}}
+			>
+				Artır
+			</button>
 			<Link to="/about">About'a Git</Link>
-			<br />
-			<a href="/about">About href</a>
 		</div>
 	);
 }
